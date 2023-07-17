005cebd5ff1c90d045eda40600e321620e6c2c33
realnav1234 <14navpreetsingh@gmail.com>
2023-07-16 22:14:48 -0400

    path fix socials

--- src/components/CompactMenu.jsx
+++ src/components/CompactMenu.jsx
@@ -9,7 +9,7 @@ export const CompactMenu = () => {
         <div className="compact-menu">
             <div className="aboutus-compactMenu"><Link smooth to="/aboutUs">about us.</Link></div>
             <div className="blog-compactMenu"><Link to="/blog">blog.</Link></div>
-            <div className="socials-compactMenu"><HashLink smooth to="/home#socials">socials.</HashLink></div>
+            <div className="socials-compactMenu"><HashLink smooth to="/#socials">socials.</HashLink></div>
             <Link to="/"><img className="FYSlogo-compactMenu" alt="Fys logo" src="/img/fys-logo-6-1.png" /></Link>
         </div>
             
