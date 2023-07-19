47ff6729f8306bdbbb5b6b8dc898097265bd24cd
realnav1234 <14navpreetsingh@gmail.com>
2023-07-18 21:59:56 -0400

    nova, nebula blurbs

--- src/screens/NebulaPage/NebulaPage.jsx
+++ src/screens/NebulaPage/NebulaPage.jsx
@@ -14,11 +14,24 @@ export const NebulaPage = () => {
             <RevealFromLeft><CompactMenu/></RevealFromLeft>
             <RevealFromRight><h1 className="h-1">team nebula</h1></RevealFromRight>
             <RevealFromLeft><p className="p">
-              Our classes are held Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-              qui officia deserunt mollit anim id est laborum.runt mollit anim id est laborum.
+              Meet Team Nebula: Future For Young Scientists' first team of educators hailing across 
+              different parts of the USA! Though each team member has been exposed to a different STEM 
+              curriculum in their schools, all are driven by the same mission: to spread their deep passion 
+              for STEM and make experiment-based lessons accessible to students across the world. Team Nebula 
+              believes that all students should be granted the opportunity to feed their inner curiosity. 
+              In fact, each Nebula educator feels that fundamental STEM concepts haven't been emphasized 
+              enough in their school districts, particularly in elementary and middle school, which creates 
+              a lack of foundation for students entering high school. Though a variety of STEM-focused educational 
+              resources are available online, Nebula feels that there is simply no substitute for experiment-based 
+              learning as well as the personal interactions offered by live lessons. Each Nebula educator is 
+              determined to spread awareness about FYS in their communities and ensure that the organization's 
+              mission is fulfilled. 
+              <br/> <br/>
+              Our classes are currently being held from August 12th to September 9th once a week on Saturdays. 
+              On the calendar below, you can find the date and time of each class, as well the name of each 
+              experiment and a materials list. The materials list for each experiment can be accessed by clicking 
+              on the date and then clicking the name of the experiment. You will be directed to a PDF containing 
+              the materials for the experiment along with relevant information and links about the experiment as well.
             </p></RevealFromLeft>
           </div>
         </div>
--- src/screens/NovaPageMobile/NovaPageMobile.jsx
+++ src/screens/NovaPageMobile/NovaPageMobile.jsx
@@ -17,11 +17,20 @@ export const NovaPageMobile = () => {
               <RevealFromRight><div className="text-wrapper-3">team nova</div></RevealFromRight>
               <RevealFromLeft>
               <p className="p">
-                Our classes are held Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-                in culpa qui officia deserunt mollit anim id est laborum.runt mollit anim id est laborum.
+              We are a group of four students from Hicksville High School who are all deeply passionate about STEM, 
+              and participate in a variety of STEM-related activities including Science Olympiad and Robotics. Our 
+              goal is to teach children from communities like ours where they may not have the money or available 
+              programs to learn science through hands-on experiments. We strive to achieve this goal by keeping our 
+              classes completely free, utilizing only common household items in our experiments, and outreaching to 
+              children from countries all around the world. We hope that through our online classes, children from 
+              around the world and from all backgrounds will get to experience science in a unique and engaging way!
+              <br/> <br/>
+              Our classes are currently being held from July 9th to August 13th. On the calendar below, you can find 
+              the date and time of each class, as well the name of each experiment and a materials list. The materials
+               list for each experiment can be accessed by clicking on the date and then clicking the name of the 
+               experiment. You will be directed to a PDF containing the materials for the experiment along with relevant 
+               information and links about the experiment as well.
+
               </p>
               </RevealFromLeft>
             </div>
