145b72eabc159d6b818d2ad30d42c71a4e80f1b3
realnav1234 <14navpreetsingh@gmail.com>
2023-07-18 22:35:43 -0400

    desktop mainpage text added

--- src/screens/MainPageDesktop/MainPageDesktop.jsx
+++ src/screens/MainPageDesktop/MainPageDesktop.jsx
@@ -18,29 +18,26 @@ export const MainPageDesktop = () => {
             <div className="overlap-group">
               <RevealFromRight><div className="text-wrapper">our classes</div></RevealFromRight>
               <p className="lorem-ipsum-dolor">
-                <RevealFromLeft><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</div></RevealFromLeft>
+                <RevealFromLeft><div> Our <b>free weekly online science classes</b> for children are geared toward children in grades 2-7 to convey advanced 
+              science topics by conducting experiments that utilize household materials, allowing us to achieve our goal 
+              in the most accessible way possible. In addition to our classes, Future for Young Scientists have held fun 
+              community events to continue our belief in bettering our local communities. In 2021, the Founders curated the 
+              Future for Young Scientists Educators Mentorship. We aim to allow other high achieving high school students to 
+              learn valuable life skills by being an educator for Future for Young Scientists. Since its conception, the Founders 
+              have mentored three teams of educators. We allow our Educators to have full autonomy for what they teach so that 
+              they are fully passionate about the wide range of STEM topics that they choose to teach.</div></RevealFromLeft>
                 <br /> <br/>
-                <RevealFromLeft><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</div></RevealFromLeft>
+                <RevealFromLeft><div>In general, our classes are held over <b>Google Meets</b> and range from 45-60 minutes. Our classes are separated 
+              into <b>2-5th graders</b> in our Elementary Focused Lesson and <b>6-7th graders</b> in our Middle School Focused Lesson. 
+              Students and parents are welcome to join our classes at <b>any point</b> in the session! This means if you find our 
+              program after we have begun you are more than welcome to fill out our registration form and join the next class.</div></RevealFromLeft>
                 <br /> <br/>
-                <RevealFromLeft><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</div></RevealFromLeft>
-                <br /><br/>
-                <RevealFromLeft><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.</div></RevealFromLeft>
+                <RevealFromLeft><div>With our Educators, the <b>Nova Team</b> have now taught for two years and are all seniors at Hicksville High School. 
+              They have held multiple in person community events in Hicksville to bring STEM education to the local community. 
+              The <b>Nebula Team</b> will be debuting this summer as our first nationwide team! They are bright high school students 
+              who are passionate about increasing the reach of Future for Young Scientists. We hope that you are able to attend 
+              classes for both of these amazing groups, see you soon!</div></RevealFromLeft>
+                <br />
                 
               </p>
             </div>
@@ -50,11 +47,11 @@ export const MainPageDesktop = () => {
               <RevealFromRight><div className="text-wrapper-2">welcome</div></RevealFromRight>
               <RevealFromRight>
               <p className="welcome-message">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+              Future for Young Scientists is an organization that seeks to spread the knowledge 
+            of STEM to children by indulging in fun and engaging at-home experiments. With the 
+            program running since 2020, across our different educators the organization has seen 
+            nearly 200 different students since then from all across the globe. We hope to continue 
+            to foster growth in our young scientists so explore our website to learn more!
               </p>
               </RevealFromRight>
             </div>
@@ -86,24 +83,25 @@ export const MainPageDesktop = () => {
           <div className="overlap-3">
             <RevealFromLeft><div className="text-wrapper-10">parent testimonials</div></RevealFromLeft>
             <RevealFromRight><div><p className="text-wrapper-11">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat.
+            "Great Job!  The experiments were easy to perform but scientifically relevant and well-explained!  
+              Class discussion was good, engaging the students."
+              <br/>
+              <b>— Douglas E.S.</b>
             </p></div></RevealFromRight>
             <RevealFromRight><div><p className="text-wrapper-12">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat.
+            “You all rock, guys! So interesting, entertaining and overall you all are so patient and sweet with kids. Thank you all so much!”
+              <br/> <b>— Aida D.</b>
             </p></div></RevealFromRight>
             <RevealFromRight><div><p className="text-wrapper-13">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat.
+            “Thank you for everything. The kids are excited to hear you will continue to have these experiments. 
+              What you are providing to these young ones are priceless experiences and memories. Thank you again!”
+              <br/> <b>— Amy H.</b>
             </p></div></RevealFromRight>
             <RevealFromRight><div><p className="text-wrapper-14">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat.
+            “Future Young Scientists offered a wonderful program that my 10 year old son enjoyed this summer. 
+              The experiments were fun and educational. They utilized common household items and the clean up was easy. 
+              I highly recommend this program for elementary and middle school students.”
+              <br/> <b>— Marilyn P.K.</b>
             </p></div></RevealFromRight>
           </div>
         </div>
